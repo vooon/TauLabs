f477ec4b205105861be6dbd53b0172075d5e82c7
Vladimir Ermakov <vooon341@gmail.com>
2013-11-17 15:14:10 +0400

    cap2: Fix build after 0f9bae4

--- flight/targets/captainpro2/board-info/board-info.mk
+++ flight/targets/captainpro2/board-info/board-info.mk
@@ -1,7 +1,7 @@
-BOARD_TYPE          := 0x85
+BOARD_TYPE          := 0x84
 BOARD_REVISION      := 0x01
-BOOTLOADER_VERSION  := 0x80
-HW_TYPE             := 0x00
+BOOTLOADER_VERSION  := 0x81
+HW_TYPE             := 0x00		# seems to be unused
 
 MCU                 := cortex-m4
 CHIP                := STM32F405RGT
@@ -16,14 +16,15 @@ OPENOCD_CONFIG      := stm32f4xx.stlink.cfg
 BL_BANK_BASE        := 0x08000000  # Start of bootloader flash
 BL_BANK_SIZE        := 0x00008000  # Should include BD_INFO region (32kb)
 
-# Leave the remaining 16KB and 64KB sectors for other uses
+EE_BANK_BASE        := 0x08008000
+EE_BANK_SIZE        := 0x00008000
+
+# Leave the remaining 64KB sector for other uses
+
 FW_BANK_BASE        := 0x08020000  # Start of firmware flash (128kb)
 FW_BANK_SIZE        := 0x00040000  # Should include FW_DESC_SIZE (256kb)
 
 FW_DESC_SIZE        := 0x00000064
-
-EE_BANK_BASE        := 0x00000000
-EE_BANK_SIZE        := 0x00000000
 
 EF_BANK_BASE        := 0x08000000  # Start of entire flash image (usually start of bootloader as well)
 EF_BANK_SIZE        := 0x00060000  # Size of the entire flash image (from bootloader until end of firmware)
